b79ad1825734bdea19ab37b3498268d11600665c
Fellipe Leonardo <112525075+rakRandom@users.noreply.github.com>
2024-04-18 20:29:47 -0300

    updating
    new features: dot struct, user-defined dot quantity, better gravity point movement, more accurate simulation and more options (disable gravity).

--- gravity_simulator.cpp
+++ gravity_simulator.cpp
@@ -6,11 +6,16 @@
 #include "raylib/raylib.h"
 #include "raylib/raymath.h"
 
+struct Dot
+{
+    Vector2 position;
+    Vector2 velocity;
+} Dot;
 
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
-int main(void)
+int main(int argc, char *argv[])
 {
     // Initialization
     //--------------------------------------------------------------------------------------
@@ -18,6 +23,7 @@ int main(void)
 
     const double gravityForce = 5000000;
     const double gravityPointSpeed = 50;
+    const double speedLimit = 7500;
     const int screenWidth = 1080;
     const int screenHeight = 720;
     const std::string title("RayLib - Gravity Simulator");
@@ -35,49 +41,53 @@ int main(void)
     Vector2 scaledGravityPoint;
     Vector2 move;
     double distance;
-    double gravityDirection = 1;
+    double gravityDirection = 0;
 
     // Dots variables
-    size_t vectorListSize = 200;
-    Vector2 *vectorList = (Vector2*) std::malloc(vectorListSize * sizeof(Vector2));
-    Vector2 *velocityList = (Vector2*) std::malloc(vectorListSize * sizeof(Vector2));
+    size_t dotQuantity;
+    if (argc == 1)
+        dotQuantity = 100;
+    else
+        dotQuantity = std::stoi(argv[1]);
 
-    for (size_t i = 0; i < vectorListSize; i++) 
+    struct Dot *dotArray = (struct Dot *)std::malloc(dotQuantity * sizeof(struct Dot));
+
+    for (size_t i = 0; i < dotQuantity; i++)
     {
-        vectorList[i] = Vector2{(float) (rand() % screenWidth) * 100, (float) (rand() % screenHeight) * 100};
-        velocityList[i] = Vector2Zero();
+        dotArray[i].position = Vector2{(float)(rand() % screenWidth) * 100, (float)(rand() % screenHeight) * 100};
+        dotArray[i].velocity = Vector2Zero();
     }
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    SetTargetFPS(60); // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
     // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    while (!WindowShouldClose()) // Detect window close button or ESC key
     {
         // Event Get
         //----------------------------------------------------------------------------------
         // Toggle to show general details
         if (IsKeyReleased(KEY_INSERT))
-            showGeneralDetails = (uint8_t) (showGeneralDetails == 0);
-        
+            showGeneralDetails = (uint8_t)(showGeneralDetails == 0);
+
         // Toggle to show gravity details
         if (IsKeyReleased(KEY_DELETE))
-            showGravityDetails = (uint8_t) (showGravityDetails == 0);
-        
-        // Toggles if the gravity will attract or repel
+            showGravityDetails = (uint8_t)(showGravityDetails == 0);
+
+        // Toggles the gravity mode - -1 to 0 to 1 to -1
         if (IsKeyReleased(KEY_R))
-            gravityDirection *= -1;
+            gravityDirection = (gravityDirection == 0) ? 1 : ((gravityDirection == 1) ? -1 : 0);
 
         // Toggle if the gravity point is based on the mouse position or not
-        if (IsKeyReleased(KEY_P)) 
+        if (IsKeyReleased(KEY_P))
         {
-            useMouseAsGravityPoint = (uint8_t) (useMouseAsGravityPoint == 0);
+            useMouseAsGravityPoint = (uint8_t)(useMouseAsGravityPoint == 0);
             gravityPoint = Vector2{screenWidth / 2, screenHeight / 2};
         }
 
         // Toggle Dark Mode
         if (IsKeyReleased(KEY_K))
-            useDarkMode = (uint8_t) (useDarkMode == 0);
+            useDarkMode = (uint8_t)(useDarkMode == 0);
 
         //----------------------------------------------------------------------------------
 
@@ -87,159 +97,135 @@ int main(void)
         // Mouse-related gravity point calculation
         if (useMouseAsGravityPoint)
             gravityPoint = GetMousePosition();
-        
-        // Not-mouse-related gravity point movement
-        else 
-        {
-            if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) 
-            {
-                gravityPoint.x -= gravityPointSpeed * GetFrameTime();
 
-                if (gravityPoint.x < screenWidth / 2 - gravityPointSpeed)
-                    gravityPoint.x = screenWidth / 2 + gravityPointSpeed;
-            }
-            else if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) 
-            {
+        // Not-mouse-related gravity point movement
+        else
+        {
+            // Horizontal movement
+            if (IsKeyDown(KEY_A))
+                gravityPoint.x -= gravityPointSpeed * GetFrameTime();
+            if (IsKeyDown(KEY_D))
                 gravityPoint.x += gravityPointSpeed * GetFrameTime();
 
-                if (gravityPoint.x > screenWidth / 2 + gravityPointSpeed)
-                    gravityPoint.x = screenWidth / 2 - gravityPointSpeed;
-            }
-            else if (IsMouseButtonDown(MOUSE_BUTTON_MIDDLE))
+            // Vertical movement
+            if (IsKeyDown(KEY_W))
+                gravityPoint.y -= gravityPointSpeed * GetFrameTime();
+            if (IsKeyDown(KEY_S))
+                gravityPoint.y += gravityPointSpeed * GetFrameTime();
+
+            // Go back to default position
+            if (IsKeyDown(KEY_H))
                 gravityPoint.x = screenWidth / 2;
         }
-        
-        
 
         // Velocity calculation
-        for (size_t i = 0; i < vectorListSize; i++) 
+        for (size_t i = 0; i < dotQuantity; i++)
         {
             scaledGravityPoint = Vector2Scale(gravityPoint, 100);
-            distance = Vector2Distance(scaledGravityPoint, vectorList[i]);
+            distance = Vector2Distance(scaledGravityPoint, dotArray[i].position);
 
             if (!(distance < 99 && distance > -99))
             {
-                move = Vector2MoveTowards(vectorList[i], scaledGravityPoint, 1);
+                move = Vector2MoveTowards(dotArray[i].position, scaledGravityPoint, 1);
 
-                velocityList[i] = Vector2Add(
+                dotArray[i].velocity = Vector2Add(
                     Vector2Scale(
                         Vector2Normalize(
                             Vector2Subtract(
-                                move, 
-                                vectorList[i]
-                            )
-                        ), 
-                        (gravityForce * GetFrameTime() * gravityDirection) / (distance / 2)
-                    ),
-                    velocityList[i]
-                ); 
+                                move,
+                                dotArray[i].position)),
+                        (gravityForce * GetFrameTime() * gravityDirection) / (distance / 2)),
+                    dotArray[i].velocity);
 
-                if (velocityList[i].x > 5000)
-                    velocityList[i].x = 5000;
-                else if (velocityList[i].x < -5000)
-                    velocityList[i].x = -5000;
-                
-                if (velocityList[i].y > 5000)
-                    velocityList[i].y = 5000;
-                else if (velocityList[i].y < -5000)
-                    velocityList[i].y = -5000;
+                if (Vector2Length(dotArray[i].velocity) > speedLimit)
+                    dotArray[i].velocity = Vector2Scale(Vector2Normalize(dotArray[i].velocity), speedLimit);
             }
         }
 
         // Position update
-        for (size_t i = 0; i < vectorListSize; i++) 
+        for (size_t i = 0; i < dotQuantity; i++)
         {
-            vectorList[i] = Vector2Add(vectorList[i], Vector2Scale(velocityList[i], GetFrameTime()));
+            dotArray[i].position = Vector2Add(dotArray[i].position, Vector2Scale(dotArray[i].velocity, GetFrameTime()));
         }
-        
+
         //----------------------------------------------------------------------------------
 
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
-            
-            if (useDarkMode)
-            {
-                ClearBackground(BLACK);
 
-                for (size_t i = 0; i < vectorListSize; i++) 
-                    DrawCircleV(Vector2Scale(vectorList[i], 0.01), 2.0f, Color{(unsigned char) (Vector2Length(velocityList[i]) * 235 / 7100 + 20), 0, 0, 255});
-                
-                DrawText("DARK MODE: ON", screenWidth - 140, 5, 18, GREEN);
-            }
-            else
-            {
-                ClearBackground(RAYWHITE);
+        if (useDarkMode)
+        {
+            ClearBackground(BLACK);
 
-                for (size_t i = 0; i < vectorListSize; i++) 
-                    DrawCircleV(Vector2Scale(vectorList[i], 0.01), 2.0f, BLACK);
-            }
+            for (size_t i = 0; i < dotQuantity; i++)
+                DrawCircleV(Vector2Scale(dotArray[i].position, 0.01), 2.0f, Color{(unsigned char)(Vector2Length(dotArray[i].velocity) * 200 / speedLimit + 55), 0, 0, 255});
 
-            // Draw the dots
-            DrawCircleV(gravityPoint, 2.0f, BLUE);
+            DrawText("DARK MODE: ON", screenWidth - 140, 5, 18, GREEN);
+        }
+        else
+        {
+            ClearBackground(RAYWHITE);
 
-            // Draw the details
-            if (showGeneralDetails)
-            {
-                // General details
-                DrawText(
-                    std::string("FPS: ").append(std::to_string(GetFPS())).c_str(), 
-                    5, 5, 18, GREEN
-                );
-                DrawText(
-                    std::string("DOTS: ").append(std::to_string(vectorListSize)).c_str(), 
-                    5, 25, 18, GREEN
-                );
-            }
-            if (showGravityDetails)
-            {
-                // Gravity details
-                DrawText(
-                    std::string("GRAV. FORCE: ").append(std::to_string(gravityForce)).c_str(), 
-                    5, 55, 18, GREEN
-                );
-                DrawText(
-                    std::string("GRAV. REVERSED: ").append((gravityDirection == 1) ? "FALSE" : "TRUE").c_str(), 
-                    5, 75, 18, GREEN
-                );
-                DrawText(
-                    std::string("GRAV. POS.").c_str(), 
-                    5, 95, 18, GREEN
-                );
-                DrawText(
-                    std::string("X: ").append(std::to_string(gravityPoint.x)).c_str(), 
-                    5, 115, 18, GREEN
-                );
-                DrawText(
-                    std::string("Y: ").append(std::to_string(gravityPoint.y)).c_str(), 
-                    5, 135, 18, GREEN
-                );
+            for (size_t i = 0; i < dotQuantity; i++)
+                DrawCircleV(Vector2Scale(dotArray[i].position, 0.01), 2.0f, BLACK);
+        }
 
-                if (!useMouseAsGravityPoint)
-                    DrawText(
-                        std::string("GRAV. POINT SPEED: ").append(std::to_string(gravityPointSpeed)).c_str(), 
-                        5, 155, 18, GREEN
-                    );
-            }
+        // Draw the dots
+        DrawCircleV(gravityPoint, 2.0f, BLUE);
 
-            // Draw the keys
+        // Draw the details
+        if (showGeneralDetails)
+        {
+            // General details
             DrawText(
-                "INSERT - Info. 1 Toggle | DELETE - Info. 2 Toggle | R - Grav. Force Toggle | P - Grav. Point Toggle | K - Theme Toggle",
-                5,
-                screenHeight - 14,
-                14,
-                GREEN
-            );
-            
+                std::string("FPS: ").append(std::to_string(GetFPS())).c_str(),
+                5, 5, 18, GREEN);
+            DrawText(
+                std::string("DOTS: ").append(std::to_string(dotQuantity)).c_str(),
+                5, 25, 18, GREEN);
+        }
+        if (showGravityDetails)
+        {
+            // Gravity details
+            DrawText(
+                std::string("GRAV. FORCE: ").append(std::to_string(gravityForce)).c_str(),
+                5, 55, 18, GREEN);
+            DrawText(
+                std::string("GRAV. REVERSED: ").append((gravityDirection == 0) ? "DISABLED" : ((gravityDirection == 1) ? "NORMAL" : "REVERSED")).c_str(),
+                5, 75, 18, GREEN);
+            DrawText(
+                std::string("GRAV. POS.").c_str(),
+                5, 95, 18, GREEN);
+            DrawText(
+                std::string("X: ").append(std::to_string(gravityPoint.x)).c_str(),
+                5, 115, 18, GREEN);
+            DrawText(
+                std::string("Y: ").append(std::to_string(gravityPoint.y)).c_str(),
+                5, 135, 18, GREEN);
+
+            if (!useMouseAsGravityPoint)
+                DrawText(
+                    std::string("GRAV. POINT SPEED: ").append(std::to_string(gravityPointSpeed)).c_str(),
+                    5, 155, 18, GREEN);
+        }
+
+        // Draw the keys
+        DrawText(
+            "INSERT - Info. 1 Toggle | DELETE - Info. 2 Toggle | R - Grav. Mode Toggle | P - Grav. Point Toggle | K - Theme Toggle",
+            5,
+            screenHeight - 14,
+            14,
+            GREEN);
+
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
-    free(vectorList);
-    free(velocityList);
-    CloseWindow();        // Close window and OpenGL context
+    free(dotArray);
+    CloseWindow(); // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
 
     return 0;
